6d22852ef660781aeaca5fe580835a4a9b88efb4
Kolya Kopylov <yanikolya13@gmail.com>
2022-06-06 00:06:16 +0300

    Добавьте файлы проекта.

---
+++ flynergeniy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace flynergeniy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
